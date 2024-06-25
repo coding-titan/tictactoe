b0441b8a23f92c76e5634d4a8ba622cbf35db1d5
Seb <sebastian.manieri@gmail.com>
2024-06-25 14:16:12 -0700

    First commit of main

---
+++ main.py
@@ -0,0 +1,7 @@
+# Tic Tac Toe in the terminal
+
+available_coordinates = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"]
+
+coordinate_dict = {"a1": "n", "a2": "n", "a3": "n", "b1": "n", "b2": "n", "b3": "n", "c1": "n", "c2": "n", "c3": "n"}
+
+print(coordinate_dict)
