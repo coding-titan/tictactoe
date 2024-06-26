f2e51f39bf9dd6be79fa839d88756053ce6dd9f8
Seb <sebastian.manieri@gmail.com>
2024-06-26 10:23:46 -0700

    working win check with generator expression

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Tic Tac Toe in the terminal
 
 import time
+import random
 
 coordinate_dict = {"a1": " ", "a2": " ", "a3": " ", "b1": " ", "b2": " ", "b3": " ", "c1": " ", "c2": " ", "c3": " "}
 
@@ -15,7 +16,7 @@ win_combinations = [
     ("a3", "b2", "c1")
 ]
 
-def win_check(coordinate_dict, player):
+def win_check(player):
     x_list = []
     o_list = []
     if player == "p1":
@@ -23,15 +24,30 @@ def win_check(coordinate_dict, player):
             if value.lower() == "x":
                 x_list.append(key)
         for combination in win_combinations:
-            if combination[0] and combination[1] and combination[2] in x_list:
-                print(current_playfield)
+            if all(combo in x_list for combo in combination):
+                current_playfield = update_playfield()
                 time.sleep(1)
                 print("Player 1 wins!")
                 time.sleep(3)
                 exit()
+                
+def p1_play():
+    player_input = input("Place your cross: ")
+    while player_input not in coordinate_dict.keys():
+        player_input = input("Not a valid input - place your cross or type \"quit\" to exit")
+    if player_input.lower() == "quit":
+        exit()
+    coordinate_dict[player_input.lower()] = "X"
 
-while True:
-    #Update playfield
+def ai_play():
+    avaiable_coordinates = []
+    for key, value in coordinate_dict.items():
+        if not value == "X" and not value == "O":
+            avaiable_coordinates.append(key)
+    choice = random.choice(avaiable_coordinates)
+    coordinate_dict[choice] = "O"
+    
+def update_playfield():
     current_playfield = f"""
       1   2   3
     A {coordinate_dict["a1"]} | {coordinate_dict["a2"]} | {coordinate_dict["a3"]} |
@@ -41,12 +57,17 @@ while True:
     C {coordinate_dict["c1"]} | {coordinate_dict["c2"]} | {coordinate_dict["c3"]} |
     """
     print(current_playfield)
+    return current_playfield
+
+while True:
+    #Update playfield
+    current_playfield = update_playfield()
     #Player input
-    player_input = input("Place your cross: ")
-    while player_input not in coordinate_dict.keys():
-        player_input = input("Not a valid input - place your cross or type \"quit\" to exit")
-    if player_input.lower() == "quit":
-        exit()
-    coordinate_dict[player_input.lower()] = "X"
-    #Win check for player
-    win_check(coordinate_dict, "p1")
+    p1_play()
+    win_check("p1")
+    current_playfield = update_playfield()
+    
+    ai_play()
+    win_check("p2")
+    current_playfield = update_playfield()
+    
